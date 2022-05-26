4c01c8cab3271c388ffe539ff005790e36b064ab
farabi1038 <ibnfarabishihab@gmail.com>
2022-05-26 10:56:48 -0400

    tutorials commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/farabi/Desktop/rosCode/src
+# Build directory: /home/farabi/Desktop/rosCode/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("beginner_tutorials")
---
+++ build/beginner_tutorials/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/farabi/Desktop/rosCode/src/beginner_tutorials
+# Build directory: /home/farabi/Desktop/rosCode/build/beginner_tutorials
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
